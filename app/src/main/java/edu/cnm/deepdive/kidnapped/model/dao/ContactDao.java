9ba0e81d6d8a0de9c64a1d387d5c961a03361cc5
isaac <shifdub@gmail.com>
2020-11-04 17:33:45 -0700

    worked on daos

--- app/src/main/java/edu/cnm/deepdive/kidnapped/model/dao/ContactDao.java
+++ app/src/main/java/edu/cnm/deepdive/kidnapped/model/dao/ContactDao.java
@@ -1,5 +1,43 @@
 package edu.cnm.deepdive.kidnapped.model.dao;
 
+import android.provider.ContactsContract.Contacts;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+import edu.cnm.deepdive.kidnapped.model.entity.Contact;
+import io.reactivex.Single;
+import java.util.Collection;
+import java.util.List;
+
+@Dao
 public interface ContactDao {
 
+  @Insert
+  Single<Long> insert (Contact contact);
+
+  @Insert
+  Single<List<Long>> insert (Contact... contacts);
+
+  @Insert
+  Single<List<Long>> insert (Collection<Contact> contacts);
+
+  @Update
+  Single<Integer> update (Contact contact);
+
+  @Update
+  Single<Integer> update (Contact... contacts);
+
+  @Update
+  Single<Integer> update (Collection<Contact> contacts);
+
+  @Delete
+  Single<Integer> delete (Contact contact);
+
+  @Delete
+  Single<Integer> delete (Contacts... contacts);
+
+  @Delete
+  Single<Integer> delete (Collection<Contact> contacts);
 }
+
