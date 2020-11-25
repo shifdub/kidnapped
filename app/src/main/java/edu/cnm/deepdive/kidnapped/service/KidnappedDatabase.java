6ea71bfdd94140c115ac67a73d515e28a214d5a6
isaac <shifdub@gmail.com>
2020-11-25 08:40:38 -0700

    updated depepndencies. ..

--- app/src/main/java/edu/cnm/deepdive/kidnapped/service/KidnappedDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/kidnapped/service/KidnappedDatabase.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverters;
 import edu.cnm.deepdive.kidnapped.model.dao.ContactDao;
 import edu.cnm.deepdive.kidnapped.model.dao.UserDao;
 import edu.cnm.deepdive.kidnapped.model.entity.Contact;
