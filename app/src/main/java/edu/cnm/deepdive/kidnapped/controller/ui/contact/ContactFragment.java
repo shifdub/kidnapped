39260ac6acc83ef0aef0cb28ba7ea9b2341f773a
isaac <shifdub@gmail.com>
2020-12-01 15:06:07 -0700

    updated colors. designing record passphrase screen

--- app/src/main/java/edu/cnm/deepdive/kidnapped/controller/ui/contact/ContactFragment.java
+++ app/src/main/java/edu/cnm/deepdive/kidnapped/controller/ui/contact/ContactFragment.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.kidnapped.controller.ui.contact;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
