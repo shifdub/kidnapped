002b731254447690ffeeb1c0e859971246418509
isaac <shifdub@gmail.com>
2020-12-03 12:15:48 -0700

    set save contact button in Contact Fragment.

--- app/src/main/java/edu/cnm/deepdive/kidnapped/controller/ui/contact/ContactFragment.java
+++ app/src/main/java/edu/cnm/deepdive/kidnapped/controller/ui/contact/ContactFragment.java
@@ -1,17 +1,15 @@
 package edu.cnm.deepdive.kidnapped.controller.ui.contact;
 
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.kidnapped.R;
+import edu.cnm.deepdive.kidnapped.model.entity.Contact;
 
 public class ContactFragment extends Fragment {
 
@@ -19,16 +17,19 @@ public class ContactFragment extends Fragment {
 
   public View onCreateView(@NonNull LayoutInflater inflater,
       ViewGroup container, Bundle savedInstanceState) {
-    contactViewModel =
-        ViewModelProviders.of(this).get(ContactViewModel.class);
     View root = inflater.inflate(R.layout.fragment_contact, container, false);
-    final TextView textView = root.findViewById(R.id.text_contact);
-    contactViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-      @Override
-      public void onChanged(@Nullable String s) {
-        textView.setText(s);
-      }
+    root.findViewById(R.id.saveContactButton).setOnClickListener((v) -> {
+      Contact contact = new Contact();
+      //TODO Populate the fields of contact with contents of view objects on the screen.
+      contactViewModel.save(contact);
     });
     return root;
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view,
+      @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    contactViewModel = new ViewModelProvider(this).get(ContactViewModel.class);
   }
 }
--- app/src/main/java/edu/cnm/deepdive/kidnapped/controller/ui/contact/ContactViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/kidnapped/controller/ui/contact/ContactViewModel.java
@@ -1,19 +1,37 @@
 package edu.cnm.deepdive.kidnapped.controller.ui.contact;
 
+import android.app.Application;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import edu.cnm.deepdive.kidnapped.model.entity.Contact;
+import edu.cnm.deepdive.kidnapped.service.ContactRepository;
+import io.reactivex.disposables.CompositeDisposable;
 
-public class ContactViewModel extends ViewModel {
+public class ContactViewModel extends AndroidViewModel {
 
-  private MutableLiveData<String> mText;
+  private final ContactRepository contactRepository;
+  private final MutableLiveData<Throwable> throwable;
+  private final CompositeDisposable pending;
 
-  public ContactViewModel() {
-    mText = new MutableLiveData<>();
-
+  public ContactViewModel(
+      @NonNull Application application) {
+    super(application);
+    contactRepository = new ContactRepository(application);
+    throwable = new MutableLiveData<>();
+    pending = new CompositeDisposable();
   }
 
-  public LiveData<String> getText() {
-    return mText;
+  public void save(Contact contact) {
+    throwable.setValue(null);
+    pending.add(
+        contactRepository.save(contact)
+            .subscribe(
+                () -> {},
+                throwable::postValue
+            )
+    );
   }
 }
